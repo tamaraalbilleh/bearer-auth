3679bc2f39994adff7ca78ea67589690a5e7d0d2
tamaraalbilleh <tamara2albella@gmail.com>
2021-05-25 23:24:48 +0300

    for testing and deployment

---
+++ src/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes.js');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/v1',authRoutes);//
+
+app.get ('/', homePageHandler); //
+function homePageHandler (req,res){
+  res.send ('welcome to home page!');
+}
+
+
+// Catchalls
+app.use('*', notFound);//
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  startup: (port) => {
+    app.listen(port, () => {
+      console.log(`Server Up on ${port}`);
+    });
+  },
+};
+
